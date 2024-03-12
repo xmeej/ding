c1cedb46db61ceb88c53aff2fa32868a556ec635
xmeej <163122720+xmeej@users.noreply.github.com>
2024-03-12 22:01:19 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -39,7 +39,7 @@ let CMproxyIPs = [
 let BotToken ='';
 let ChatID =''; 
 let proxyhosts = [//本地代理域名池
-	//'dingyueqi.688818.xyz/',
+	//'/',
 ];
 let proxyhostsURL = 'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/proxyhosts';//在线代理域名池URL
 let EndPS = '老八云收集';//节点名备注内容
