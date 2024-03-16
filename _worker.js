28d02f6ecd5a145250832d14759286c34f50ffc9
xmeej <163122720+xmeej@users.noreply.github.com>
2024-03-16 22:57:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,7 +17,8 @@ let addressesapi = [
 
 let DLS = 4;//速度下限
 let addressescsv = [
-	'https://raw.githubusercontent.com/xmeej/yxip/main/8075-1710171531.csv', //iptest测速结果文件。
+	'https://raw.githubusercontent.com/xmeej/yxip/main/8075-1710171531.csv', 
+	'https://raw.githubusercontent.com/xmeej/ding/main/addressescsv.csv',//iptest测速结果文件。
 ];
 
 let subconverter = "dingyueqi.688818.xyz"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
