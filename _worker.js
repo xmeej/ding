98ec3dc2c888dc6dc29ff407f7bc3bbaf3484ee5
xmeej <163122720+xmeej@users.noreply.github.com>
2024-03-16 18:22:06 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -21,7 +21,7 @@ let addressescsv = [
 	'https://raw.githubusercontent.com/xmeej/yxip/main/8075-1710171531.csv',	
 ];
 
-let subconverter = "dingyueqi.688818.xyz"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subconverter = "api.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
 
 let link = '';
