a635efe330cd575d7b1cec59128c1ab59c85f745
xmeej <163122720+xmeej@users.noreply.github.com>
2024-03-17 00:18:26 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,18 +11,18 @@ let addresses = [
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://raw.githubusercontent.com/xmeej/yxip/main/ip.txt',
+	//'https://raw.githubusercontent.com/xmeej/yxip/main/ip.txt',
 	'https://addressesapi.090227.xyz/CloudFlareYes',
 	'https://raw.githubusercontent.com/xmeej/ding/main/addressesapi.txt',//可参考内容格式 自行搭建。
 ];
 
 let DLS = 4;//速度下限
 let addressescsv = [
-	'https://raw.githubusercontent.com/xmeej/yxip/main/8075-1710171531.csv', 
+	//'https://raw.githubusercontent.com/xmeej/yxip/main/8075-1710171531.csv', 
 	'https://raw.githubusercontent.com/xmeej/ding/main/addressescsv.csv',//iptest测速结果文件。
 ];
 
-let subconverter = "dingyueqi.688818.xyz"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subconverter = "api.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
 
 let link = '';
